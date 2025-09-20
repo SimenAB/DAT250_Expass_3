131b83b8e24c4edb84caafaa7ca5ab29d279b225
SimenAB <76975587+SimenAB@users.noreply.github.com>
2025-09-20 03:30:25 +0200

    First commit for expass 4
    Branched from expass 3
    Added dependencies
    Removed springboot dependency
    Created PollsTest, added testcode

---
+++ dat250-expass4.md
@@ -0,0 +1,18 @@
+<h2> Experiment Assignment 3</h2>
+
+### The things I did
+- *Link to code*
+- *How you inspected the database tables and what tables were created, for latter; provide screenshots*
+
+### What I still need to do
+- *pending issues with the assignment*
+
+### Technical issues
+- *Encountered during installation and use of JPA *
+
+Checklist:
+Added dependencies
+implementation("org.springframework.boot:spring-boot-starter-web") from build.gradle
+created PollsTest, added test code
+
+
--- expass1_spring_boot/build.gradle.kts
+++ expass1_spring_boot/build.gradle.kts
@@ -19,12 +19,16 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     // Lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+    // Hibernate - H2
+    implementation("org.hibernate.orm:hibernate-core:7.1.1.Final")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
+    implementation("com.h2database:h2:2.3.232")
 
 }
 
